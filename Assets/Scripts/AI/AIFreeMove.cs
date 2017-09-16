a0604f38cef46ed18aefaa772f5df96cd7e4813b
midorun <midoruton0906@gmail.com>
2017-09-16 23:00:20 +0900

    自由移動・ノード移動を追加

---
+++ Assets/Scripts/AI/AIFreeMove.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+public class AIFreeMove : MonoBehaviour {
+    private Vector3 _movingDiretion = Vector3.right;
+    [SerializeField]private float speed = 0.1f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void FixedUpdate(){
+		RaycastHit hit = new RaycastHit();
+		int mask = ~(1 << 8);
+        Physics.Raycast(this.transform.position, _movingDiretion, out hit, Mathf.Infinity,mask);
+        if (hit.collider != null && hit.distance < 8f){
+            _movingDiretion = GetNextDirection();
+        }
+        this.transform.position += _movingDiretion *speed;
+    }
+    private Vector3 GetNextDirection(){
+		RaycastHit hit = new RaycastHit();
+		int mask = ~(1 << 8);
+		var rayDirection = Vector3.right;
+		var vecList = new List<Vector2>();
+		for (int i = 0; i < 72; i++)
+		{
+			Physics.Raycast(this.transform.position, rayDirection, out hit, Mathf.Infinity, mask);
+            if(hit.collider == null){
+  
+                return rayDirection;
+            }
+
+            vecList.Add(new Vector2(i, hit.distance));
+			rayDirection = Quaternion.Euler(0, 5f, 0) * rayDirection;
+		}
+        float index = vecList.Where(v=>v.y == vecList.Max(m=>m.y)).ElementAt(0).x;
+        return Quaternion.Euler(0, 5f * index, 0) * Vector3.right;
+    }
+}
---
+++ Assets/Scripts/AI/AINodeMove.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEditor;
+public class AINodeMove : MonoBehaviour {
+    public List<Transform> _transformList;
+    private List<Vector3> _positionList;
+    private int _index = 0;
+    [SerializeField] private float speed;
+	// Use this for initialization
+	void Start () {
+        _positionList = new List<Vector3>(_transformList.Select(t=>t.position));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+
+        if(_index<_positionList.Count){
+            Vector3 nextPosition = _positionList[_index];
+            if (Vector3.Distance(this.transform.position, nextPosition) < 1f){
+                _index++;
+            }
+            this.transform.position += (nextPosition - this.transform.position).normalized * speed;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if(Selection.gameObjects.Count()==1&&!Application.isPlaying){
+ 
+            var selectedGameObject = Selection.gameObjects[0];
+            if (selectedGameObject == this.gameObject || selectedGameObject.transform.parent == this.gameObject.transform)
+            {
+				
+				Gizmos.color = Color.blue;
+                Gizmos.DrawLine(this.transform.position,_transformList[0].position);
+                for (int i = 0; i < _transformList.Count();i++)
+                {
+                    Gizmos.color = Color.red;
+                    Gizmos.DrawSphere(_transformList[i].position, 1f);
+                    if (i + 1 < _transformList.Count())
+                    {
+                        Gizmos.color = Color.blue;
+                        Gizmos.DrawLine(_transformList[i].position, _transformList[i + 1].position);
+                    }
+                }
+
+            }
+        }
+
+    }
+
+}
+
