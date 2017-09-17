aaaad600f39fdb8ba78ab8b1f8e5dcac7d80a601
stakumi <saito25000@outlook.com>
2017-09-17 15:05:06 +0900

    シーン繋げました

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Main : MonoBehaviour {
+
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.G)) {
+			SceneManager.LoadScene (3);
+		}
+		if (Input.GetKeyDown (KeyCode.C)) {
+			SceneManager.LoadScene (4);
+		}
+	}
+}
