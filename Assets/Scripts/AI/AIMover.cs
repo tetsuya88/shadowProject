2f39000994bdbf9199aa02296086604f894c01a6
midorun <midoruton0906@gmail.com>
2017-09-17 16:18:55 +0900

    IAImoverをいじった

--- Assets/Scripts/AI/AIMover.cs
+++ Assets/Scripts/AI/AIMover.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 [AddComponentMenu("AIScript/AIMover")]
 public class AIMover : MonoBehaviour
 {
+    
     private IAIMoveStrategy aiMoveStrategy;
     // Use this for initialization
     void Awake()
@@ -23,5 +24,16 @@ public class AIMover : MonoBehaviour
     private void FixedUpdate()
     {
         aiMoveStrategy.DoMove();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(LayerMask.LayerToName(other.gameObject.layer)=="Bat"){
+            var prevStrategy = aiMoveStrategy;
+            aiMoveStrategy = null;
+            aiMoveStrategy = this.gameObject.AddComponent<AIEscapeMove>();
+            aiMoveStrategy.SetSpeed(prevStrategy.GetSpeed());
+            (aiMoveStrategy as AIEscapeMove).SetMoveDirection(Vector3Utiltiy.ReturnNormalizedYZeroVec3(this.transform.position - other.transform.position));
+        }
     }
 }
