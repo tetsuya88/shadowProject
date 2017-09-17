fb1144a794270c546e6924e15c62afd7cce35bb9
stakumi <saito25000@outlook.com>
2017-09-17 14:01:17 +0900

    とりあえずシーンつくりました

--- Assets/Scripts/PlayerSkill.cs
+++ Assets/Scripts/PlayerSkill.cs
@@ -15,6 +15,7 @@ public class PlayerSkill : MonoBehaviour {
 	private Vector3 startpos;
 	private float dis = 0f;
 	private Vector3 batpos;
+	private bool flag=true;
 
 	void Start () {
 		
@@ -22,28 +23,30 @@ public class PlayerSkill : MonoBehaviour {
 
 	void Update () {
 		startpos = transform.forward * minpos+ new Vector3(0,-1f,0);
-		if (Input.GetKeyDown (KeyCode.Space)) {
+		if (Input.GetKeyDown (KeyCode.Space)&&flag) {
+			flag = false;
 			dis = 0f;
 			batcircle = Instantiate (batCircle) as GameObject;
-			batcircle.transform.position = transform.position + transform.forward*dis + startpos;
-		}else if (Input.GetKeyUp (KeyCode.Space)) {
+			batcircle.transform.position = transform.position + transform.forward * dis + startpos;
+		} else if (Input.GetKeyUp (KeyCode.Space)&&batcircle!=null) {
 			batpos = batcircle.transform.position;
 			Destroy (batcircle);
 			StartCoroutine ("BatStart");
-		}else if(Input.GetKey(KeyCode.Space)){
-			if (dis < maxpos-minpos) {
+		} else if (Input.GetKey (KeyCode.Space)&&batcircle!=null) {
+			if (dis < maxpos - minpos) {
 				dis += Time.deltaTime * speed;
 			} 
 			batcircle.transform.position = transform.position + transform.forward * dis + startpos;
 		}
+
 	}
 
 	IEnumerator BatStart(){
 		bat = Instantiate (Bat) as GameObject;
 		bat.transform.position = batpos;
-		Debug.Log ("bat");
 		yield return new WaitForSeconds (2);
 		Destroy (bat);
+		flag = true;
 	}
 
 }
