ebe210a50198fd98da48505c7ded2a8652673623
stakumi <saito25000@outlook.com>
2017-09-18 14:23:52 +0900

    ステージ草案

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -20,17 +20,19 @@ public class PlayerMove : MonoBehaviour {
 		if (Input.GetButton ("Horizontal") || Input.GetButton ("Vertical")) {
 			x = Input.GetAxis ("Horizontal");
 			z = Input.GetAxis ("Vertical");
+			/*
             var cameraPosition = Camera.main.transform.position;
             var forwardVec = new Vector3((this.transform.position - cameraPosition).x, 0f,(this.transform.position - cameraPosition).z);
             forwardVec = forwardVec.normalized;
             var rightVec = Vector3.Cross(forwardVec, Vector3.up);
 
             this.transform.forward = z * forwardVec + -1f*x*rightVec;
-            rb.MovePosition(transform.position + z*Time.deltaTime * forwardVec*speed + -1f*x *Time.deltaTime * rightVec*speed);
-            //rb.MovePosition (transform.position + new Vector3 (x * Time.deltaTime * speed, 0f, z * Time.deltaTime * speed));
+			rb.MovePosition(transform.position + z*Time.deltaTime * forwardVec*speed + -1f*x *Time.deltaTime * rightVec*speed);
+			*/
+			rb.MovePosition (transform.position + new Vector3 (x * Time.deltaTime * speed, 0f, z * Time.deltaTime * speed));
             if (x != 0 || z != 0)
             {
-                transform.rotation = Quaternion.LookRotation(-1f*(rightVec * x) + forwardVec*z);
+				transform.rotation = Quaternion.LookRotation(transform.position+Vector3.right * x + Vector3.forward * z -transform.position);
 			}
 		}
 	}
