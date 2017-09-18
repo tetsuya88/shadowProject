a8c14363fab78bc120e5354089ac7f8aaee272e5
stakumi <saito25000@outlook.com>
2017-09-18 21:22:44 +0900

    UImawari

--- Assets/Scripts/00_Title/TitleButton.cs
+++ Assets/Scripts/00_Title/TitleButton.cs
@@ -5,7 +5,14 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class TitleButton : MonoBehaviour {
-	public void OnClick(){
-		SceneManager.LoadScene (1);
+	public float waittime = 2f;
+	void Update(){
+		if (waittime < 0) {
+			if (Input.GetKeyDown (KeyCode.Return)) {
+				SceneManager.LoadScene (1);
+			}
+		} else {
+			waittime -= Time.deltaTime;
+		}
 	}
 }
--- Assets/Scripts/01_Loading/Loading.cs
+++ Assets/Scripts/01_Loading/Loading.cs
@@ -1,15 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class Loading : MonoBehaviour {
-	public float loadTime = 5f;
+	public float loadTime = 6f;
+	public Text text;
+
 
 	void Update () {
 		loadTime -= Time.deltaTime;
 		if (loadTime < 0) {
-			SceneManager.LoadScene (2);
+			text.text = "Press Enter";
+			if (Input.GetKeyDown(KeyCode.Return)) {
+				SceneManager.LoadScene (2);
+			}
+		}else if (loadTime < 1) {
+			text.text = "Now Loading    ";
+		}else if (loadTime < 2) {
+			text.text = "Now Loading ...";
+		}else if (loadTime < 3) {
+			text.text = "Now Loading .. ";
+		}else if (loadTime < 4) {
+			text.text = "Now Loading .  ";
+		}else if (loadTime < 5) {
+			text.text = "Now Loading    ";
 		}
 	}
 }
