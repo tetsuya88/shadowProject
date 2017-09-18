7c46f0ca4d9bdfb1753c22c945354fb6a21c4a5c
stakumi <saito25000@outlook.com>
2017-09-18 20:10:26 +0900

    スポナー追加

--- Assets/Scripts/VillagerSpawner.cs
+++ Assets/Scripts/VillagerSpawner.cs
@@ -3,25 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class VillagerSpawner : MonoBehaviour {
-
-	private int num = 0; 
-	private bool flag = 0;
+ 
+	private bool flag = true;
 	private float timecount = 3f;
 
 	public int max = 10;
 	public GameObject obasan;
 	public GameObject syonen;
+	GameObject[] nodes;
 
 	void Start () {
-		
+		nodes = GameObject.FindGameObjectsWithTag ("Node");
 	}
 
 	void Update () {
-		timecount
+		timecount -= Time.deltaTime;
 		if(timecount <0){
+			timecount = 3f;
 			GameObject[] villagers = GameObject.FindGameObjectsWithTag ("Person");
 			if (villagers.Length < max) {
-				if()
+				int index = Random.Range (0, nodes.Length);
+				if (flag) {
+					flag = false;
+					//Instantiate (obasan, nodes[index].transform.position);
+				} else {
+					flag = true;
+				//	Instantiate (syonen, nodes[index].transform.position);
+				}
 			}
 		}
 	}
