1b3c0d28282c98f1685bcc4f1383355c907cedc7
midorun <midoruton0906@gmail.com>
2017-09-18 11:08:55 +0900

    コミット

--- Assets/SetBuildTileEasy.cs
+++ Assets/SetBuildTileEasy.cs
@@ -21,12 +21,19 @@ public class SetBuildTileEasy : MonoBehaviour {
             collider = gameObject.AddComponent<BoxCollider>();
         }
 
-        collider.center = new Vector3((float)_x / 2f, (float)transform.childCount / 2f + 1f, (float)_z / 2f);
-        collider.size = new Vector3((float)_x, (float)transform.childCount, (float)_z);
+        collider.center = new Vector3(((float)_x / 2f)*transform.lossyScale.x, ((float)transform.childCount / 2f)* transform.lossyScale.y , ((float)_z / 2f)*transform.lossyScale.z);
+        collider.size = new Vector3(((float)_x)*transform.lossyScale.x, ((float)transform.childCount)* transform.lossyScale.y, ((float)_z)*transform.lossyScale.z);
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (Application.isPlaying) return;
+
+        for (int i = 0; i < this.transform.childCount;i++){
+            this.transform.GetChild(i).transform.localPosition = new Vector3(0f, (float)(i * this.transform.lossyScale.y), 0f);
+            Debug.Log(this.transform.childCount);
+        }
+
 	}
 }
